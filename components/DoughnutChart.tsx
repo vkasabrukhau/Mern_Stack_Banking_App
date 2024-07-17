239d5623432f0c36e7db80a1c6f1d10d7343aa8d
Val Kasabrukhau <vkasabrukhau@Vals-MacBook-Pro.local>
2024-07-17 13:37:45 -0400

    Added the Sidebar component and the Mobilenav component. The mobile nav component is currently buggy and incomplete.

--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -16,7 +16,16 @@ const DoughnutChart = ({accounts}: DoughnutChartProps) => {
         ],
         labels: ['Bank 1', 'Bank 2', 'Bank 3']
     }
-    return <Doughnut data={data}/>
+    return <Doughnut 
+    data={data}
+    options={{
+        cutout: '60%',
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }}/>
 }
 
 export default DoughnutChart
