9e4a2cfa4bf3082573906fbfa89af73c0dd6f944
Val Kasabrukhau <vkasabrukhau@Vals-MacBook-Pro.local>
2024-07-18 14:12:29 -0400

    Completed the mobile nav bar and the side bar all the way. Large colorful nav bar for opposite side coming soon.

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -19,52 +19,60 @@ import { cn } from "@/lib/utils"
 import { sidebarLinks } from '@/constants'
 
 const MobileNav = ({user}: MobileNavProps) => {
-    const pathname = usePathname();
+  const pathname = usePathname();
   return (
     <section className="w-full max-w-[264px]">
         <Sheet>
             <SheetTrigger>
-                <Image src="/icons/hamburger.svg" width={24} height={24} alt="menu" className="cursor-pointer"/>
+                <Image
+                src="/icons/hamburger.svg"
+                width={30}
+                height={30}
+                alt="menu"
+                className="cursor-pointer"
+                />
             </SheetTrigger>
             <SheetContent side="left" className="border-none bg-white">
                 <Link href="/" className='flex cursor-pointer items-center gap-1 px-4'>
-                            <Image
-                                src="/icons/logo.svg"
-                                width={34}
-                                height={34}
-                                alt="Horizon Bank"
-                            />
-                            <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Horizon Bank"
+                    />
+                    <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>Horizon</h1>
                 </Link>
-                    <div className="mobilenav-sheet">
-                        <SheetClose asChild key={item.route}>
-                            <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                <div className="mobilenav-sheet">
+                    <SheetClose asChild>
+                        <nav className="flex h-full flex-col gap-6 pt-16 text-white">
                             {sidebarLinks.map((item) => {
                                 const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                                 return (
-                                    <Link href={item.route} key={item.label} className={cn('mobilenav-sheet_close w-full', {'bg-bank-gradient': isActive})}>
-                                            <Image
-                                                src={item.imgURL}
-                                                alt={item.label}
-                                                width={20}
-                                                height={20}
-                                                className={cn({
-                                                    'brightness-[3] invert-0': isActive
-                                                })}
-                                            />
-                                        <p className={cn(
-                                            'sidebar-label',
-                                            {
-                                                '!text-white' :isActive
-                                            })}>
-                                            {item.label}
-                                        </p>
-                                    </Link>
+                                    <SheetClose asChild key={item.route}>
+                                        <Link href={item.route} key={item.label} className={cn('mobilenav-sheet_close w-full', {'bg-bank-gradient': isActive})}>
+                                                <Image
+                                                    src={item.imgURL}
+                                                    alt={item.label}
+                                                    width={20}
+                                                    height={20}
+                                                    className={cn({
+                                                        'brightness-[3] invert-0': isActive
+                                                    })}
+                                                />
+                                            <p className={cn(
+                                                'text-16 font-semibold text-black-2',
+                                                {
+                                                    'text-white' :isActive
+                                                })}>
+                                                {item.label}
+                                            </p>
+                                        </Link>
+                                    </SheetClose>
                                 )
                             })}
-                            </nav>
-                        </SheetClose>
-                    </div>
+                        </nav>
+                    </SheetClose>
+                </div>
             </SheetContent>
         </Sheet>
     </section>
