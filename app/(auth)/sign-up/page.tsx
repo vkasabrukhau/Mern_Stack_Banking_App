f48ac237f9cedf42b838765079525bc226fe23ca
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-07-22 10:33:53 -0400

    Added a new form component

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import AuthForm from '@/components/AuthForm';
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+    return (
+        <section className='flex-center size-full max-sm:p-6'>
+            <AuthForm type="sign-up"/>
+        </section>
+    )
 }
 
 export default SignUp
