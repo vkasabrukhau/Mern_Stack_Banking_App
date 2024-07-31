9eeb1c71a200d309dd34e833564fddcc732eb8ab
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-07-31 07:06:08 -0400

    Added a dynamic transactions table along with styling, access token error still persists

--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -9,10 +9,27 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import {
+  cn,
   formatAmount,
+  formatDateTime,
   getTransactionStatus,
   removeSpecialCharacters,
 } from "@/lib/utils";
+import { transactionCategoryStyles } from "@/constants";
+
+const CategoryBadge = ({ category }: CategoryBadgeProps) => {
+  const { borderColor, backgroundColor, textColor, chipBackgroundColor } =
+    transactionCategoryStyles[
+      category as keyof typeof transactionCategoryStyles
+    ] || transactionCategoryStyles.default;
+  return (
+    <div className={cn("category-badge, borderColor, chipBackgroundColor")}>
+      <div className={cn("size-2 rounded-full", backgroundColor)}>
+        <p className={cn("text-[12px] font-medium", textColor)}>{category}</p>
+      </div>
+    </div>
+  );
+};
 
 const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return (
@@ -37,14 +54,37 @@ const TransactionsTable = ({ transactions }: TransactionTableProps) => {
           const isCredit = t.type === "credit";
 
           return (
-            <TableRow key={t.id}>
-              <TableCell>
-                <div>
-                  <h1>{removeSpecialCharacters(t.name)}</h1>
+            <TableRow
+              key={t.id}
+              className={`${
+                isDebit || amount[0] === "-" ? "bg-[#FFFBFA]" : "bg-[#F6FEF9]"
+              } !over:bg-none !border-b-DEFAULT`}
+            >
+              <TableCell className="max-w-[250px] pl-2 pr-10">
+                <div className="flex items-center gap-3">
+                  <h1 className="text-14 truncate font-semibold text-[#344054]">
+                    {removeSpecialCharacters(t.name)}
+                  </h1>
                 </div>
               </TableCell>
-              <TableCell>
+              <TableCell
+                className={`pl-2 pr-10 font-semibold ${
+                  isDebit || amount[0] === "-" ? "bg-[#f04438]" : "bg-[#039855]"
+                }`}
+              >
                 {isDebit ? `-${amount}` : isCredit ? amount : amount}
+              </TableCell>
+              <TableCell className="pl-2 pr-10">
+                <CategoryBadge category={status} />
+              </TableCell>
+              <TableCell className="pl-2 pr-10 min-w-32">
+                {formatDateTime(new Date(t.date)).dateTime}
+              </TableCell>
+              <TableCell className="pl-2 pr-10 capitalize min-w-24">
+                {t.paymentChannel}
+              </TableCell>
+              <TableCell className="pl-2 pr-10 max-md:hidden">
+                <CategoryBadge category={t.category} />
               </TableCell>
             </TableRow>
           );
