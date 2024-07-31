366c57c45cd2b41b2dbcdbcfb4714e74e957a845
Valiantsin Kasabrukhau <vkasabrukhau@gmail.com>
2024-07-31 13:52:19 -0400

    Modified so static pages aren't an issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const ibmPlexSerif = IBM_Plex_Serif({
   subsets: ["latin"],
-  variable: '--font-ibm-plex-serif',
-  weight: ["400", '700'], // 400 is the regular weight, 700 is the bold weight
+  variable: "--font-ibm-plex-serif",
+  weight: ["400", "700"], // 400 is the regular weight, 700 is the bold weight
   display: "swap", // "swap" means the font will load asynchronously, "block" means it will load synchronously
-})
-
+});
 
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platofmr for everyone",
   icons: {
-    icon: '/icons/logo.svg'
-  }
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
